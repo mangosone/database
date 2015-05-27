1c67353eaf7af1756fc365a03ad8384a89976956
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-05-27 13:03:17 +0100

    Caretaker Dilandrus - HellFire
    Missing text added
    
    SD2 script linked

---
+++ World/Updates/Rel21/21000_09_Caretaker_Dilandrus.sql
@@ -0,0 +1,69 @@
+-- --------------------------------------------------------------------------------
+-- This is an attempt to create a full transactional update
+-- --------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `update_mangos`; 
+
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_mangos`()
+BEGIN
+    DECLARE bRollback BOOL  DEFAULT FALSE ;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `bRollback` = TRUE;
+
+  SET @cOldRev = 'required_21000_08_Rathis_Tomber_vendor'; 
+
+  -- Set the new revision string
+  SET @cNewRev = 'required_21000_09_Caretaker_Dilandrus';
+
+  -- Set thisRevision to the column name of db_version in the currently selected database
+  SET @cThisRev := ((SELECT column_name FROM information_schema.`COLUMNS` WHERE table_name='db_version' AND table_schema=(SELECT DATABASE() AS thisDB FROM DUAL) AND column_name LIKE 'required%'));
+
+ 
+  -- Only Proceed if the old values match
+  IF @cThisRev = @cOldRev THEN
+    -- Make this all a single transaction
+    START TRANSACTION;
+
+    -- Apply the Version Change from Old Version to New Version
+    SET @query = CONCAT('ALTER TABLE db_version CHANGE COLUMN ',@cOldRev, ' ' ,@cNewRev,' bit;');
+    PREPARE stmt1 FROM @query;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
+    -- The Above block is required for making table changes
+
+    -- -- -- -- Normal Update / Insert / Delete statements will go here  -- -- -- -- --
+          
+    -- ================ Caretaker Dilandrus - missing text, and SD2 script linked ================ --
+          
+    -- correct text
+    -- http://wowwiki.wikia.com/Caretaker_Dilandrus
+    UPDATE npc_text SET text0_0 = 'I was born here, $r. This is my world - all that I know... As a boy, my mother and father would regale me with tales of your world and the splendor it held. How I longed to leave this place.$B$BAlas, fate is a cruel mistress. My mother and father have long since passed, buried in this graveyard. I remain to uphold their honor and the honor of my family - The Sons of Lothar.' WHERE ID = 8676;
+
+    -- link to script (visiting graves)
+    -- http://www.wowhead.com/npc=16856/caretaker-dilandrus#comments
+    DELETE FROM script_binding WHERE bind = 16856;
+    INSERT INTO script_binding (type, ScriptName, bind, data) VALUES (0, 'npc_caretaker_dilandrus', 16856, 0);
+
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+    
+    -- If we get here ok, commit the changes
+    IF bRollback = TRUE THEN
+      ROLLBACK;
+      SELECT '* UPDATE FAILED *' AS 'Status',@cThisRev AS 'DB is on Version';
+    ELSE
+      COMMIT;
+      SELECT '* UPDATE COMPLETE *' AS 'Status',@cNewRev AS 'DB is now on Version';
+    END IF;
+  ELSE
+    SELECT '* UPDATE SKIPPED *' AS 'Status',@cOldRev AS 'Required Version',@cThisRev AS 'Found Version';
+  END IF;
+
+END $$
+
+DELIMITER ;
+
+-- Execute the procedure
+CALL update_mangos();
+
+-- Drop the procedure
+DROP PROCEDURE IF EXISTS `update_mangos`;
