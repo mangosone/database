c5b8d3b10176525aa72056fbd8ec34c31fe71200
Antz <billy1arm@users.noreply.github.com>
2016-03-23 07:39:42 +0000

    [CharDB] match client limits. Base DB updated to Rel21_2_1
    Also fixed all Errors and Warnings in Base Character DB
    
    Thanks for @stfx for the initial patch

--- Character/Setup/characterCreateDB.sql
+++ Character/Setup/characterCreateDB.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `character1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE USER 'mangos'@'localhost' IDENTIFIED BY 'mangos';
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES ON `characters`.* TO 'mangos'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, LOCK TABLES ON `character1`.* TO 'mangos'@'localhost';
