e304f7a20b2867b7d5a70a475e8c351d9230ee91
Antz <billy1arm@users.noreply.github.com>
2020-12-29 22:59:36 +0000

    Happy New Year 2021 from everyone at getMangos.eu :tada:

--- make_full_WorldDB.sh
+++ make_full_WorldDB.sh
@@ -1,6 +1,6 @@
 cat <<EOF > full_db.sql
 --
--- Copyright (C) 2005-2021 MaNGOS <https://getmangos.eu/> <https://github.com/mangoszero>
+-- Copyright (C) 2005-2021 MaNGOS <https://getmangos.eu/> <https://github.com/mangosone>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
