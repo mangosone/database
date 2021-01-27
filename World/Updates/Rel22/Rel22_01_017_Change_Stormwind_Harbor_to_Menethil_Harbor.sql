323b67ea0f751f37c0830124fe730fe63d0322e2
i-am-fyre <58180427+i-am-fyre@users.noreply.github.com>
2021-01-26 20:14:01 -0600

    Update Rel22_01_017_Change_Stormwind_Harbor_to_Menethil_Harbor.sql

--- World/Updates/Rel22/Rel22_01_017_Change_Stormwind_Harbor_to_Menethil_Harbor.sql
+++ World/Updates/Rel22/Rel22_01_017_Change_Stormwind_Harbor_to_Menethil_Harbor.sql
@@ -43,7 +43,7 @@ BEGIN
         -- -- PLACE UPDATE SQL BELOW -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
-UPDATE `npc_text` SET `text0_0` = "On the northern dock, you can board a ship that will carry you to Rut'theran Village and Darnassus.  From the southern dock, you can find passage across the Great Sea to Menethil Harbor.  The dock to the west, at the end of the pier, leads to Azuremyst Isle, near the Exodar. Safe journeys to you!" WHERE `ID`=5480;
+UPDATE `npc_text` SET `text0_0` = "On the northern dock, you can board a ship that will carry you to Rut'theran Village and Darnassus.  From the southern dock, you can find passage across the Great Sea to Menethil Harbor on Khaz Modan.  The dock to the west, at the end of the pier, leads to Azuremyst Isle, near the Exodar. Safe journeys to you!" WHERE `ID`=5480;
 
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
         -- -- PLACE UPDATE SQL ABOVE -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
